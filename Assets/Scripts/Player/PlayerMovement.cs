5695e1be9f40a8f2ffdc0a886156b1671879b4a5
alejandro escarpa <escarpaprieto@gmail.com>
2023-09-19 17:45:00 +0200

    Player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables
+
+    #region Inspector variables
+    [Header("Variables")]
+    [SerializeField] float movementSpeed = 10f;
+    [SerializeField] float runSpeed = 10f;
+    #endregion
+
+    #region Private Variables
+    private Rigidbody2D playerRb;
+    private Vector2 moveInput;
+    #endregion
+
+    #endregion
+
+    #region Unity Methods
+    private void Awake()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(moveX, moveY).normalized;
     }
+
+    private void LateUpdate()
+    {
+        playerRb.MovePosition(playerRb.position + moveInput * movementSpeed * Time.fixedDeltaTime);
+    }
+    #endregion
+
+    #region Inputs events
+    
+    #endregion
 }
