f306cfa866713af93b9f06385cbc79119024ff66
Florin <fkorincat1@gmail.com>
2023-09-24 01:14:03 +0200

    Not Finished Turret placing

--- Assets/Scripts/HUD/TorretsHUD.cs
+++ Assets/Scripts/HUD/TorretsHUD.cs
@@ -7,12 +7,18 @@ public class TorretsHUD : MonoBehaviour
 {
     [SerializeField] private Canvas canvas;
     [SerializeField] private GameObject HUD;
+    [SerializeField] private GameObject Torrets;
     private Transform initialParent;
+    private InteractiveWorld world;
 
 
-    private void Awake()
+    private void Start()
     {
+        Debug.Log("esto es el padre "+this.transform.parent.gameObject);
+        Debug.Log("hola");
         initialParent=this.transform.parent;
+        world = this.transform.parent.gameObject.transform.parent.gameObject.GetComponent<TorretsManager>().IW;
+        //world = GetComponent<InteractiveWorld>();
     }
     public void DragHandler(BaseEventData data)
     {
@@ -25,12 +31,14 @@ public class TorretsHUD : MonoBehaviour
         HUD.gameObject.SetActive(false);
         this.transform.parent=canvas.transform;
         this.gameObject.SetActive(true);
-
     }
     public void DropHandler()
     {
         this.transform.parent = initialParent;
         transform.position = this.transform.parent.position;
+        GameObject aux = (GameObject)Instantiate(Torrets, new Vector3(0, 0, 0), Quaternion.identity);
+        Debug.Log(aux);
+        world.Construct(aux);
     }
 
 }
