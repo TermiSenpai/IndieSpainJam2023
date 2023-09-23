07fe0bc160577ecb62aa262949173d011a1593cf
alejandro escarpa <escarpaprieto@gmail.com>
2023-09-23 22:13:13 +0200

    Player OnDeathZoom

--- Assets/Scripts/Manager/PlayerDeathManager.cs
+++ Assets/Scripts/Manager/PlayerDeathManager.cs
@@ -1,18 +1,52 @@
+using Cinemachine;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDeathManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CinemachineVirtualCamera playerCamera;
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float centerSpeed;
+    [SerializeField] CinemachineFramingTransposer framingTransposer;
+
+    private void OnEnable()
     {
-        
+        PlayerHealth.PlayerDeathRelease += OnPlayerDeath;
+    }
+    private void OnDisable()
+    {
+        PlayerHealth.PlayerDeathRelease -= OnPlayerDeath;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnPlayerDeath()
     {
-        
+        CenterCamera();
+        StartCoroutine(CameraZoom());
+    }
+
+    void CenterCamera()
+    {
+
+    }
+
+    private IEnumerator CameraZoom()
+    {
+        // Obtiene el componente CinemachineFramingTransposer de la Virtual Camera.
+        framingTransposer = playerCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+
+        while (playerCamera.m_Lens.OrthographicSize > 2)
+        {
+            playerCamera.m_Lens.OrthographicSize -= Time.deltaTime * zoomSpeed;
+
+            if (framingTransposer != null && framingTransposer.m_DeadZoneHeight > 0)
+            {
+                // Centra la cámara en el personaje.
+
+                framingTransposer.m_DeadZoneWidth -= Time.deltaTime;
+                framingTransposer.m_DeadZoneHeight -= Time.deltaTime;
+            }
+
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
     }
 }
