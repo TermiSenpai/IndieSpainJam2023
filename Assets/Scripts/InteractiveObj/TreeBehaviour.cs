cf6af423290240675378e95508b4a97c3e6f5446
Florin <fkorincat1@gmail.com>
2023-09-24 15:13:37 +0200

    Shake

--- Assets/Scripts/InteractiveObj/TreeBehaviour.cs
+++ Assets/Scripts/InteractiveObj/TreeBehaviour.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public enum TreeType
@@ -12,7 +13,11 @@ public class TreeBehaviour : MonoBehaviour
     private bool mousePressed = false;
     private bool mouseOver = false;
     private float count = 0;
+    private Vector3 posIni;
     [SerializeField] private float timer = 5;
+
+    float speed = 1.0f; //how fast it shakes
+    float amount = 0.1f; //how much it shakes
     //[SerializeField] private TreeType treeType = TreeType.Arbol_1;
 
     private void Start()
@@ -22,7 +27,7 @@ public class TreeBehaviour : MonoBehaviour
         Debug.Log(treeType.ToString());*/
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if ((!mouseOver) || (!mousePressed))
         {
@@ -36,11 +41,23 @@ public class TreeBehaviour : MonoBehaviour
         }
         else
         {
-            Debug.Log("Clicking");
-            count += Time.deltaTime;
-
+            
+            StartCoroutine(Tremble());
+            //transform.position= new Vector2(transform.position.x+(Mathf.Sin(Time.time * speed) * amount),transform.position.y);
         }
 
+    }
+    IEnumerator Tremble()
+    {
+        //for (int i = 0; i < 10; i++)
+       // {
+            Debug.Log("Clicking");
+            count += Time.deltaTime;
+            transform.localPosition += new Vector3(0.1f, 0, 0);
+            yield return new WaitForSeconds(0.01f);
+            transform.localPosition -= new Vector3(0.1f, 0, 0);
+            yield return new WaitForSeconds(0.01f);
+        //}
     }
 
     void OnMouseDown()
