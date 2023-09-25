94f0622ecb691915cbbea4acb5d17ed01e1c82d7
Jije <guillez3198@gmail.com>
2023-09-25 16:50:53 +0200

    Teletransporte

---
+++ Assets/Scripts/TeletransportePlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeletransportePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    [SerializeField] GameObject m_PlayerPrefab;
+    [SerializeField] Transform m_Camp;
+
+    void Start()
+    {
+        
+    }
+
+    public void Campamento()
+    {
+        m_PlayerPrefab.GetComponent<Transform>().position = m_Camp.position;
+
+    }
+}
