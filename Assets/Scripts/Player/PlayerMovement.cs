4fe50865bb154cc48dfcc2879fc3f9004512a785
TermiSenpai <escarpaprieto@gmail.com>
2024-04-24 09:42:59 +0200

    Player movement abstract

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,52 +1,17 @@
+
 using UnityEngine;
 
-
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : IMovementStrategy
 {
-    #region Variables
+    private readonly Rigidbody2D playerRb;
 
-    #region Inspector variables
-    [Header("Variables")]
-    [SerializeField] PlayerStats stats;
-    #endregion
-
-    #region Private Variables
-    private Rigidbody2D playerRb;
-    private Vector2 moveInput;
-    private Animator playerAnimator;
-    #endregion
-
-    #endregion
-
-    #region Unity Methods
-    private void Awake()
+    public PlayerMovement(Rigidbody2D rb)
     {
-        playerRb = GetComponent<Rigidbody2D>();
-        playerAnimator = GetComponent<Animator>();
+        playerRb = rb;
     }
 
-    private void Update()
+    public void Move(Vector2 direction)
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        moveInput = new Vector2(moveX, moveY).normalized;
-
-        if (moveInput != Vector2.zero)
-        {
-            playerAnimator.SetFloat("Horizontal", moveX);
-            playerAnimator.SetFloat("Vertical", moveY);
-        }
-
-        if (moveInput != Vector2.zero)
-            playerAnimator.SetBool("IsMoving", true);
-        else
-            playerAnimator.SetBool("IsMoving", false);
+        playerRb.MovePosition(playerRb.position + direction * Time.fixedDeltaTime * PlayerStats.Instance.moveSpeed);
     }
-
-    private void LateUpdate()
-    {
-        playerRb.MovePosition(playerRb.position + stats.moveSpeed * Time.fixedDeltaTime * moveInput);
-    }
-    #endregion
 }
