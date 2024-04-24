86a0f480479a1e2ddff2984d1767bc29f97be1b7
TermiSenpai <escarpaprieto@gmail.com>
2024-04-24 10:29:03 +0200

    Player state comments

--- Assets/Scripts/Player/PlayerStateManager.cs
+++ Assets/Scripts/Player/PlayerStateManager.cs
@@ -1,21 +1,23 @@
 using UnityEngine;
 
+// Enum to represent the different states of the player
 public enum PlayerState
 {
-    Recolection,
-    Build,
-    Combat
+    Recolection, // State for recolection phase
+    Build,       // State for build phase
+    Combat       // State for combat phase
 }
 
 public class PlayerStateManager : MonoBehaviour
 {
     public static PlayerStateManager Instance;
 
+    // References to the scripts controlling different player states
     [SerializeField] private MonoBehaviour recolectionScript;
     [SerializeField] private MonoBehaviour buildScript;
     [SerializeField] private MonoBehaviour combatScript;
 
-    private PlayerState currentState = PlayerState.Recolection;
+    private PlayerState currentState = PlayerState.Recolection; // Current state of the player
 
     private void Awake()
     {
@@ -24,6 +26,7 @@ public class PlayerStateManager : MonoBehaviour
 
     private void OnEnable()
     {
+        // Subscribing to events triggered by DayCycle for different times of the day
         DayCycle.DayStartRelease += OnStartDay;
         DayCycle.EveningStartRelease += OnStartEvening;
         DayCycle.NightStartRelease += OnStartNight;
@@ -31,33 +34,42 @@ public class PlayerStateManager : MonoBehaviour
 
     private void OnDisable()
     {
+        // Unsubscribing from events when the object is disabled or destroyed
         DayCycle.DayStartRelease -= OnStartDay;
         DayCycle.EveningStartRelease -= OnStartEvening;
         DayCycle.NightStartRelease -= OnStartNight;
     }
 
+    // Event handlers for different times of the day
     private void OnStartDay()
     {
+        // Switching to recolection phase
         SwitchState(PlayerState.Recolection);
     }
 
     private void OnStartEvening()
     {
+        // Switching to build phase
         SwitchState(PlayerState.Build);
     }
 
     private void OnStartNight()
     {
+        // Switching to combat phase
         SwitchState(PlayerState.Combat);
     }
 
+    // Method to switch between player states
     public void SwitchState(PlayerState newState)
     {
+        // Disabling current state before switching
         DisableCurrentState();
         currentState = newState;
+        // Enabling new state after switching
         EnableCurrentState();
     }
 
+    // Method to disable the current player state
     private void DisableCurrentState()
     {
         switch (currentState)
@@ -74,6 +86,7 @@ public class PlayerStateManager : MonoBehaviour
         }
     }
 
+    // Method to enable the current player state
     private void EnableCurrentState()
     {
         switch (currentState)
@@ -90,6 +103,7 @@ public class PlayerStateManager : MonoBehaviour
         }
     }
 
+    // Method to toggle all player states on or off (used for menu open/close)
     public void OnMenuOpen(bool state)
     {
         recolectionScript.enabled = state;
