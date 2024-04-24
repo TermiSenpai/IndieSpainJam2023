cccd86360bb18426f7fe519b166435c7e3af017a
TermiSenpai <escarpaprieto@gmail.com>
2024-04-24 10:17:28 +0200

    Player state manager refactorized

--- Assets/Scripts/Player/PlayerStateManager.cs
+++ Assets/Scripts/Player/PlayerStateManager.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
 
+public enum PlayerState
+{
+    Recolection,
+    Build,
+    Combat
+}
+
 public class PlayerStateManager : MonoBehaviour
 {
     public static PlayerStateManager Instance;
-    [SerializeField] private MonoBehaviour combatScript;
-     public bool canBuild =true;
-     public bool canRecolection =true;
 
+    [SerializeField] private MonoBehaviour recolectionScript;
+    [SerializeField] private MonoBehaviour buildScript;
+    [SerializeField] private MonoBehaviour combatScript;
+
+    private PlayerState currentState = PlayerState.Recolection;
 
     private void Awake()
     {
         Instance = this;
     }
+
     private void OnEnable()
     {
         DayCycle.DayStartRelease += OnStartDay;
@@ -28,33 +38,62 @@ public class PlayerStateManager : MonoBehaviour
 
     private void OnStartDay()
     {
-        // Start recolection phase
-        //recolectionScript.enabled = true;
-        combatScript.enabled = false;
-        //buildScript.enabled = false;
+        SwitchState(PlayerState.Recolection);
     }
 
     private void OnStartEvening()
     {
-        //recolectionScript.enabled = false;
-        combatScript.enabled = false;
-        // Start build phase
-        //buildScript.enabled = true;
+        SwitchState(PlayerState.Build);
     }
 
     private void OnStartNight()
     {
-//recolectionScript.enabled = false;
-        // Start combat phase
-        combatScript.enabled = true;
-        //buildScript.enabled = false;
+        SwitchState(PlayerState.Combat);
+    }
+
+    public void SwitchState(PlayerState newState)
+    {
+        DisableCurrentState();
+        currentState = newState;
+        EnableCurrentState();
+    }
+
+    private void DisableCurrentState()
+    {
+        switch (currentState)
+        {
+            case PlayerState.Recolection:
+                recolectionScript.enabled = false;
+                break;
+            case PlayerState.Build:
+                buildScript.enabled = false;
+                break;
+            case PlayerState.Combat:
+                combatScript.enabled = false;
+                break;
+        }
+    }
+
+    private void EnableCurrentState()
+    {
+        switch (currentState)
+        {
+            case PlayerState.Recolection:
+                recolectionScript.enabled = true;
+                break;
+            case PlayerState.Build:
+                buildScript.enabled = true;
+                break;
+            case PlayerState.Combat:
+                combatScript.enabled = true;
+                break;
+        }
     }
 
     public void OnMenuOpen(bool state)
     {
+        recolectionScript.enabled = state;
+        buildScript.enabled = state;
         combatScript.enabled = state;
-        //recolectionScript.enabled = state;
-       // buildScript.enabled = state;
     }
-
 }
