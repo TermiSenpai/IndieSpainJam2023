73218c3c2a7f12ad15f38e4abe3795606484f35b
TermiSenpai <escarpaprieto@gmail.com>
2024-04-24 10:11:50 +0200

    Better functions

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -11,79 +11,60 @@ public class PlayerCombat : MonoBehaviour
     private AudioSource source;
     [SerializeField] AudioClip[] clips;
     private Animator anim;
-    Vector2 mousePos;
+    private Vector2 mousePos;
     private float timerAttack;
 
     private void Start()
     {
-        // Getting necessary components and initializing variables
         source = GetComponent<AudioSource>();
         anim = GetComponent<Animator>();
         timerAttack = 0;
-
-        // Setting reference to the PlayerStats Scriptable Object
-        stats = PlayerStats.Instance;
+        stats = PlayerStats.Instance; // Setting reference to the PlayerStats Scriptable Object
     }
 
     private void Update()
     {
-        // Getting mouse position
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        // Checking for attack input
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             SwordAttack();
         }
 
-        // Checking attack delay
         CheckAttackDelay();
     }
 
     private void CheckAttackDelay()
     {
-        // Decreasing attack timer
-        timerAttack -= Time.deltaTime;
-
-        // Ensuring timer doesn't go below zero
-        if (timerAttack <= 0)
-        {
-            timerAttack = 0;
-        }
+        // Limiting timer to zero
+        timerAttack = Mathf.Max(0, timerAttack - Time.deltaTime);
     }
 
     private bool CanAttack()
     {
-        // Checking if the attack timer has reached zero and if the player can attack again
+        // Checking if the player can attack
         return timerAttack <= 0;
     }
 
     private void SwordAttack()
     {
-        // Checking if player can attack
         if (!CanAttack()) return;
 
-        // Setting attack animation direction
-        anim.SetFloat("Horizontal", mousePos.x);
-        anim.SetFloat("Vertical", mousePos.y);
-
-        // Showing attack animation and dealing damage to enemies within attack range
+        SetAttackAnimationDirection();
         StartCoroutine(VisibleAttack());
+        DealDamageToEnemies();
 
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(AttackPoint.position, stats.raidusRange, stats.enemyLayer);
-
-        foreach (Collider2D enemy in hitEnemies)
-        {
-            Debug.Log(enemy.gameObject.name);
-            IDamageable damageable = enemy.GetComponent<IDamageable>();
-            damageable?.TakeDamage(stats.damage);
-        }
-
-        // Resetting attack timer
-        timerAttack = stats.baseAttackDelay;
+        timerAttack = stats.baseAttackDelay; // Resetting attack timer
     }
 
-    IEnumerator VisibleAttack()
+    private void SetAttackAnimationDirection()
+    {
+        // Setting attack animation direction based on mouse position
+        anim.SetFloat("Horizontal", mousePos.x);
+        anim.SetFloat("Vertical", mousePos.y);
+    }
+
+    private IEnumerator VisibleAttack()
     {
         // Showing weapon attack animation and playing swing sound
         AttackPoint.gameObject.SetActive(true);
@@ -92,16 +73,32 @@ public class PlayerCombat : MonoBehaviour
         AttackPoint.gameObject.SetActive(false);
     }
 
+    private void DealDamageToEnemies()
+    {
+        // Dealing damage to enemies within attack range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(AttackPoint.position, stats.raidusRange, stats.enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log(enemy.gameObject.name);
+            IDamageable damageable = enemy.GetComponent<IDamageable>();
+            damageable?.TakeDamage(stats.damage);
+        }
+    }
+
+    private void PlaySwingSound()
+    {
+        // Playing a random swing sound
+        if (clips != null && clips.Length > 0)
+        {
+            source.PlayOneShot(TakeRandomSwingSound());
+        }
+    }
+
     private AudioClip TakeRandomSwingSound()
     {
         // Returning a random swing sound from the array
         return clips[Random.Range(0, clips.Length)];
-    }
-
-    public void PlaySwingSound()
-    {
-        // Playing a random swing sound
-        source.PlayOneShot(TakeRandomSwingSound());
     }
 
     private void OnDrawGizmos()
