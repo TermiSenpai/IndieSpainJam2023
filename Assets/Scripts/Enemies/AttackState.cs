b32089ca3419ce45887c6cd734414d0dff4a389b
TermiSenpai <escarpaprieto@gmail.com>
2024-04-29 11:39:01 +0200

    Attack state created

---
+++ Assets/Scripts/Enemies/AttackState.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AttackState : MonoBehaviour, IEnemyState
+{
+    public void EnterState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ExitState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
