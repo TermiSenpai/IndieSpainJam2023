c90328f9a9558bfac5b5204b91c3b27e966cf9e0
TermiSenpai <escarpaprieto@gmail.com>
2024-04-29 11:12:11 +0200

    Created base state machine states

---
+++ Assets/Scripts/Enemies/FollowState.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class FollowState : MonoBehaviour, IEnemyState
+{
+    public void EnterState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ExitState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
