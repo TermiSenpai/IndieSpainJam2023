5589836ea7b490dfdf7991e4687e13c47e5eee1d
TermiSenpai <escarpaprieto@gmail.com>
2024-05-15 11:04:26 +0200

    idle state

--- Assets/Scripts/Enemies/IdleState.cs
+++ Assets/Scripts/Enemies/IdleState.cs
@@ -5,6 +5,7 @@ public class IdleState : MonoBehaviour, IEnemyState
     private GameObject campfire; // Referencia al objeto que representa el campamento
     private Transform player; // Referencia al jugador
     private EnemyStateMachine stateMachine;
+    private FollowState followState;
 
     void Start()
     {
@@ -12,31 +13,30 @@ public class IdleState : MonoBehaviour, IEnemyState
         campfire = GameObject.FindGameObjectWithTag("Campfire");
         player = GameObject.FindGameObjectWithTag("Player").transform;
         stateMachine = GetComponent<EnemyStateMachine>();
+        followState = GetComponent<FollowState>();
     }
 
     public void EnterState()
     {
         // Lógica de entrada al estado Idle
-        if (campfire != null)
+        if (campfire != null || player != null)
         {
             // Si el campamento existe, cambiar al estado Follow
-            stateMachine.ChangeState(GetComponent<FollowState>());
-        }
-        else
-        {
-            // Si el campamento no existe, priorizar al jugador como objetivo
-            stateMachine.ChangeState(GetComponent<ChaseState>());
+            stateMachine.ChangeState(followState);
         }
     }
 
     public void UpdateState()
     {
-        // Lógica de actualización del estado Idle
-        // Aquí puedes agregar cualquier comportamiento adicional que el enemigo realice mientras está en estado Idle
     }
 
     public void ExitState()
     {
-        // Lógica de salida del estado Idle
+        enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        EnterState();
     }
 }
