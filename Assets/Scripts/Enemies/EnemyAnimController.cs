8fd2e6e3fcad7b8e23c33e1a11ee09ee9a479495
TermiSenpai <escarpaprieto@gmail.com>
2024-05-16 10:01:14 +0200

    anim controller body edit

--- Assets/Scripts/Enemies/EnemyAnimController.cs
+++ Assets/Scripts/Enemies/EnemyAnimController.cs
@@ -1,25 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAnimController : MonoBehaviour
 {
     private Animator animator;
 
-    private void Start()
-    {
-        // Obtener una referencia al componente Animator
-        animator = GetComponent<Animator>();
-    }
+    // Obtener una referencia al componente Animator
+    private void Awake() => animator = GetComponent<Animator>();
 
-    public void SetFloat(string parameterName, float value)
-    {
-        // Establecer el valor del float en el Animator
-        animator.SetFloat(parameterName, value);
-    }
-    public void Trigger(string parameterName)
-    {
-        // Establecer el valor del float en el Animator
-        animator.SetTrigger(parameterName);
-    }
+    // Establecer el valor del float en el Animator
+    public void SetFloat(string parameterName, float value) => animator.SetFloat(parameterName, value);
+
+    // Establecer el valor del float en el Animator
+    public void Trigger(string parameterName) => animator.SetTrigger(parameterName);
 }
