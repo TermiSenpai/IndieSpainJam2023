4e378dfb2d48ffc06302609b99ed6e210123514c
TermiSenpai <escarpaprieto@gmail.com>
2024-05-16 08:41:53 +0200

    encapsuled pivot

--- Assets/Scripts/EnemyMelePivotRotating.cs
+++ Assets/Scripts/EnemyMelePivotRotating.cs
@@ -1,16 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMelePivotRotating : MonoBehaviour
 {
     public float rotationSpeed = 45.0f;
     public float distance = 2.0f;
-    public float stopAngle = 30.0f; // Ángulo en grados para detener la rotación
+    public float stopAngle = 30.0f; // Angle in degrees to stop rotation
     Transform target;
     FollowState followState;
 
-    private bool detenerRotacion = false;
+    private bool stopRotation = false;
 
     private void Start()
     {
@@ -19,37 +17,48 @@ public class EnemyMelePivotRotating : MonoBehaviour
 
     void Update()
     {
-        target = followState.currentTarget;
+        UpdateTarget();
         if (target == null) return;
-        // Calcular la dirección hacia el ratón desde la posición del objeto.
-        Vector3 targetDirection = (target.position - transform.position).normalized;
 
-        // Calcular el ángulo de rotación en grados.
-        float angulo = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
-
-        // Aplicar la rotación al objeto.
-        transform.rotation = Quaternion.Euler(0, 0, angulo);
-
-        // Verificar si el ángulo es menor que el ángulo de parada.
-        if (Mathf.Abs(angulo) <= stopAngle)
-        {
-            detenerRotacion = true;
-        }
-        else
-        {
-            detenerRotacion = false;
-        }
-
-        // Establecer la velocidad de rotación en función de si debemos detenernos o no.
-        if (detenerRotacion)
-        {
-            rotationSpeed = 0; // Detener la rotación.
-        }
-        else
-        {
-            rotationSpeed = 45.0f; // Puedes ajustar la velocidad aquí.
-        }
+        RotateTowardsTarget();
+        CheckStopRotation();
+        SetRotationSpeed();
     }
 
+    private void UpdateTarget()
+    {
+        target = followState.currentTarget;
+    }
 
+    private void RotateTowardsTarget()
+    {
+        // Calculate the direction towards the target from the object's position.
+        Vector3 targetDirection = (target.position - transform.position).normalized;
+
+        // Calculate the rotation angle in degrees.
+        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
+
+        // Apply rotation to the object.
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    private void CheckStopRotation()
+    {
+        // Check if the angle is less than the stop angle.
+        if (Mathf.Abs(transform.rotation.eulerAngles.z) <= stopAngle)
+            stopRotation = true;
+        else
+            stopRotation = false;
+
+    }
+
+    private void SetRotationSpeed()
+    {
+        // Set the rotation speed based on whether we should stop or not.
+        if (stopRotation)
+            rotationSpeed = 0; // Stop rotation.
+        else
+            rotationSpeed = 45.0f; // enable rotation.
+
+    }
 }
